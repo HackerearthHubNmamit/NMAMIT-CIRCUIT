7409cbb01d0d1af5c5f6608b93aac0a6e90cbdc3
Saurav Kumar <67797045+rjsaurav13@users.noreply.github.com>
2021-09-03 14:09:43 +0530

    Update CountMultiples.cpp

--- Nmamit-Circuit-01/CountMultiples.cpp
+++ Nmamit-Circuit-01/CountMultiples.cpp
@@ -2,14 +2,15 @@
 using namespace std;
 int main()
 {
-int l,r,k;
+	int l,r,k;
 	cin>>l>>r>>k;//Reading the variable values
 	int count=0;//Counter to store the number of multiples
 	for(int i=l;i<=r;i++)
 	{
-		  int val=i%k;//Finding the remainder to check for the mutliple
-		  if(val==0)// Checking if the value is remainder or not
-		  	   count++;
+		int val=i%k;//Finding the remainder to check for the mutliple
+		if(val==0){// Checking if the value is remainder or not
+			count++;
+		}		
 	}
 	cout<<count<<endl;//Printing the count of multiple
 }
